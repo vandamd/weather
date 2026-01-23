11dfd23f3a496dcb18a9083c93367fa690fdbe3c
vandamd <vandamdinh71@gmail.com>
2026-01-23 23:17:19 +0000

    layout fix

--- app/search/_layout.tsx
+++ app/search/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function SearchLayout() {
 	return (
-		<Stack screenOptions={{ headerShown: false, animation: "slide_from_right" }}>
+		<Stack screenOptions={{ headerShown: false, animation: "none" }}>
 			<Stack.Screen name="results" />
 			<Stack.Screen name="weather" />
 		</Stack>
--- app/settings/_layout.tsx
+++ app/settings/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function SettingsLayout() {
 	return (
-		<Stack screenOptions={{ headerShown: false, animation: "slide_from_right" }}>
+		<Stack screenOptions={{ headerShown: false, animation: "none" }}>
 			<Stack.Screen name="temperature-unit" />
 			<Stack.Screen name="wind-speed-unit" />
 			<Stack.Screen name="precipitation-unit" />
